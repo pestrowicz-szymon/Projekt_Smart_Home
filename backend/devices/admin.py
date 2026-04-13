ab45106d8ef4f4d80169632bb16bdd6bbbbaa45e
Szymon Pestrowicz <pestrowicz.szymon@gmail.com>
2026-04-13 21:47:52 +0200

    registered models in admin panel

--- backend/devices/admin.py
+++ backend/devices/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Home, Device, SensorData, DeviceAction
 
 # Register your models here.
+admin.site.register(Home)
+admin.site.register(Device)
+admin.site.register(SensorData)
+admin.site.register(DeviceAction)
