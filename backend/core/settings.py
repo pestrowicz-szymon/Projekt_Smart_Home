1512a52ec6e0dbee48c3f385c72cda4e48c8ed91
Szymon Pestrowicz <pestrowicz.szymon@gmail.com>
2026-04-13 19:12:36 +0200

    added IsAuthenticated method and simple JWT settings of tokens

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -117,9 +117,19 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-#Additional libraries
+# Additional libraries
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
+
+# Simple JWT settings
+from datetime import timedelta
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
