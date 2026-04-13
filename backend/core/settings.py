f89fcc510c34a394151b513fc9af5da689698181
Szymon Pestrowicz <pestrowicz.szymon@gmail.com>
2026-04-13 19:01:09 +0200

    beginning of login implementation

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -116,3 +116,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+#Additional libraries
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
