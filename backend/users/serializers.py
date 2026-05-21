28f83a25b4ecd6b0b772a775e32d6602a307deb5
pestrowicz-szymon <98894570+pestrowicz-szymon@users.noreply.github.com>
2026-05-21 16:42:14 +0200

    updated api/users/me/ (#14)

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,11 +1,35 @@
-from rest_framework import serializers
 from django.contrib.auth.models import User
+from rest_framework import serializers
+
+from devices.models import HomeMember
+
+
+class HomeMembershipSerializer(serializers.ModelSerializer):
+    home = serializers.SerializerMethodField()
+
+    class Meta:
+        model = HomeMember
+        fields = ('id', 'home', 'role', 'can_manage_devices', 'created_at')
+        read_only_fields = fields
+
+    def get_home(self, obj):
+        return {
+            'id': obj.home_id,
+            'name': obj.home.name,
+            'description': obj.home.description,
+        }
 
 
 class UserSerializer(serializers.ModelSerializer):
+    home_memberships = serializers.SerializerMethodField()
+
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'first_name', 'last_name')
+        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'home_memberships')
+
+    def get_home_memberships(self, obj):
+        memberships = obj.home_memberships.select_related('home').all().order_by('created_at')
+        return HomeMembershipSerializer(memberships, many=True).data
 
 
 class RegisterSerializer(serializers.ModelSerializer):
